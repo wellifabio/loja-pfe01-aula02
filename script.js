70322067740941512ae01f45de9989a8b8bda054
wellifabio <wellifabio@hotmail.com>
2025-03-19 14:11:34 -0300

    script no git

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('oi');
+const uri = './assets/dados.json';
+var produtos = [
+    {produto:'Boné'},
+    {produto:'Camiseta'},
+    {produto:'Meia'},
+];
+
+//Carrega os dados do Mockup
+fetch(uri)
+    .then(resp => resp.json())
+    .then(resp =>{
+        produtos = resp;
+    })
+
+function mostrarCards(){
+    const main = document.querySelector('main');
+    produtos.forEach(p=>{
+        main.innerHTML += p.produto; 
+    })
+}
